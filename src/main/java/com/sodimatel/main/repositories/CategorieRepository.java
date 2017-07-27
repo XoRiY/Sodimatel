220c44a655f8057badd4d33011f0e40a3c9301a5
ramzi123ramzi <kerdoudtahar@gmail.com>
2017-07-27 09:12:28 +0200

    ajout de méthodes abstraite suplémentaires

--- src/main/java/com/sodimatel/main/repositories/CategorieRepository.java
+++ src/main/java/com/sodimatel/main/repositories/CategorieRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sodimatel.main.domain.Categorie;
 
+
 public interface CategorieRepository extends JpaRepository<Categorie, Long>{
 
+	Categorie findByLibelle(String libelle);
+	
 }
