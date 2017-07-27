456849394d0757764cf121514fa94ac2296ef670
ramzi123ramzi <kerdoudtahar@gmail.com>
2017-07-27 09:11:39 +0200

    supression de l'entity manager

--- src/main/java/com/sodimatel/main/repositories/AppareilRepositoryImpl.java
+++ src/main/java/com/sodimatel/main/repositories/AppareilRepositoryImpl.java
@@ -16,26 +16,13 @@ public class AppareilRepositoryImpl {
 	@Autowired
 	private AppareilRepository appareilRepo;
 
-	@PersistenceContext
-	EntityManager em;
-
 	public Appareil insertAppareil(Appareil appareil) {
 		appareilRepo.save(appareil);
 		return appareil;
 	}
 
 	public List<Appareil> getAppareilBy(Marque marque, Type type) {
-		return appareilRepo.findAll();
-	}
-
-	// @Interceptors(ValidationInsertInterceptor.class)
-	public List<Appareil> getBy(Marque marque, Type type) {
-
-		List<Appareil> appareils;
-		appareils = em.createNamedQuery("Appareil.getBy_Marque_Type", Appareil.class).setParameter("marque", marque)
-				.setParameter("type", type).getResultList();
-
-		return appareils;
+		return appareilRepo.findByTypeAndMarque(type, marque);
 	}
 
 	public void delete(Appareil appareil) {
@@ -44,65 +31,43 @@ public class AppareilRepositoryImpl {
 
 	public List<Appareil> getAll() {
 
-		List<Appareil> appareils;
-
-		appareils = appareilRepo.findAll();
-		// appareils =
-		// sessionFactory.getCurrentSession().createNamedQuery("Appareil.getAll",
-		// Appareil.class).getResultList();
-
-		return appareils;
+		return appareilRepo.findAll();
 
 	}
 
 	public Appareil getAppareilBy(Long idAppareil) {
 
-		Appareil appareil;
-
-		appareil = appareilRepo.findOne(idAppareil);
-
-		return appareil;
+		return appareilRepo.findOne(idAppareil);
 
 	}
 
-	public Appareil getAppareilBy(String ref_Model) {
+	public Appareil getAppareilBy(String referenceAppareil) {
 
-		Appareil appareil;
-
-		appareil = em.createNamedQuery("Appareil.getByRef", Appareil.class).setParameter("referenceAppareil", ref_Model)
-				.getSingleResult();
-
-		return appareil;
+		return appareilRepo.findByReferenceAppareil(referenceAppareil);
 
 	}
-
 
 	public List<Appareil> getAppareilBy(Marque marque) {
-		List<Appareil> appareils;
-		appareils = em.createQuery("select app from Appareil as app where app.marque =:marque", Appareil.class)
-				.setParameter("marque", marque).getResultList();
-		return appareils;
+		return appareilRepo.findByMarque(marque);
 	}
 
 	public List<Appareil> getAppareilBy(Type type) {
-		List<Appareil> appareils;
-		appareils = em.createQuery("select app from Appareil as app where app.type =:type", Appareil.class)
-				.setParameter("type", type).getResultList();
-		return appareils;
+		return appareilRepo.findByType(type);
 	}
 
-//	@Override
-//	public List<String> searchAppareil(String reference) {
-//
-//		String query = "select app.referenceAppareil from Appareil as app where app.referenceAppareil like :ref";
-//		List<String> referencesStrings;
-//		referencesStrings = sessionFactory.getCurrentSession().createQuery(query, String.class)
-//				.setParameter("ref", reference + "%").getResultList();
-//		return referencesStrings;
-//
-//	}
+	// @Override
+	// public List<String> searchAppareil(String reference) {
+	//
+	// String query = "select app.referenceAppareil from Appareil as app where
+	// app.referenceAppareil like :ref";
+	// List<String> referencesStrings;
+	// referencesStrings = sessionFactory.getCurrentSession().createQuery(query,
+	// String.class)
+	// .setParameter("ref", reference + "%").getResultList();
+	// return referencesStrings;
+	//
+	// }
 
-	
 	public void updateAppareil(Appareil appareil) {
 		appareilRepo.save(appareil);
 	}
