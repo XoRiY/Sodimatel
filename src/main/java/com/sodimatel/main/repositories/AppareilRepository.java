6d9a3dbd2164549ccaa5e0053fc50673133a3b45
ramzi123ramzi <kerdoudtahar@gmail.com>
2017-07-27 09:11:03 +0200

    quelques méthode abstraites de plus

--- src/main/java/com/sodimatel/main/repositories/AppareilRepository.java
+++ src/main/java/com/sodimatel/main/repositories/AppareilRepository.java
@@ -5,8 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sodimatel.main.domain.Appareil;
 import java.lang.String;
 import java.util.List;
+import com.sodimatel.main.domain.Marque;
+import com.sodimatel.main.domain.Type;
 
 public interface AppareilRepository extends JpaRepository<Appareil, Long>{
 
-	List<Appareil> findByReferenceAppareil(String referenceappareil);
+
+	Appareil findByReferenceAppareil(String referenceAppareil);
+	List<Appareil> findByMarque(Marque marque);
+	List<Appareil> findByType(Type type);
+	List<Appareil> findByTypeAndMarque(Type type, Marque marque);
+
 }
